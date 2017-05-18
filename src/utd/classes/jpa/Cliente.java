7ed143217f2cc02b8f1ffba2f87b5b1f36fc6628
RogerHoney <roger.honeys@gmail.com>
2017-05-18 09:13:17 -0300

    Inseção das classe Cliente e Endereço

---
+++ src/utd/classes/jpa/Cliente.java
@@ -0,0 +1,51 @@
+package utd.classes.jpa;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//estrategia de gravar o id no banco
+	private Long id;
+	private String nome;
+	private String cpf;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	
+	
+}
